577ccd1c459ee479819fe0103d1f952556802d90
cadit <cadit@DESKTOP-PCOVD1I>
2020-01-10 10:27:42 +0100

    bola en pantalla

--- src/main/java/es/isabeljaimeatienza/pong/App.java
+++ src/main/java/es/isabeljaimeatienza/pong/App.java
@@ -2,8 +2,8 @@ package es.isabeljaimeatienza.pong;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 /**
@@ -13,13 +13,20 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
-
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        
+        StackPane root = new StackPane(); //lo guardo en una variable que he llamado root (el panel)
+        var scene = new Scene(root, 640, 480);//Crea ventana de esa medida usando la variable root
         stage.setScene(scene);
         stage.show();
+        
+        Circle circleBall = new Circle(); //aquí voy a guardar una bola, con new me creo objeto circulo
+        circleBall.setCenterX(10); // obligatoriamente debe de tener una medida, double permite decimales
+        circleBall.setCenterY(30);
+        circleBall.setRadius(7);
+        //Circle circleBall2= new Circle(10, 30, 7); es otro modo de hacer la bola pero con menos líneas
+        
+        root.getChildren().add(circleBall);//los hijos hace referencia a las cosas que contiene el panel
+        
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module es.isabeljaimeatienza.pong {
+    requires javafx.controls;
+    exports es.isabeljaimeatienza.pong;
+}
